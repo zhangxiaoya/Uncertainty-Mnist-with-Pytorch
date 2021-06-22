668216623ddbe55a3d3bc1241b9c5a174de0e67a
Ryan Cheung <zhangxiaoya1989@gmail.com>
2021-06-22 15:55:39 +0800

    [Update] model.py for fix LeNet definition

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,34 +6,38 @@ import torch.nn.functional as F
 class LeNet_standard(nn.Module):
     def __init__(self):
         super(LeNet_standard, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 320)
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 
 class LeNet_dropout(nn.Module):
     def __init__(self):
         super(LeNet_dropout, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(F.dropout(self.conv1(x), training=True), 2))
-        x = F.relu(F.max_pool2d(F.dropout(self.conv2(x), training=True), 2))
-        x = x.view(-1, 320)
+        x = F.max_pool2d(F.dropout(F.relu(self.conv1(x)), training=True), 2)
+        x = F.max_pool2d(F.dropout(F.relu(self.conv2(x)), training=True), 2)
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=True)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, training=True)
+        x = self.fc3(x)
         return x
